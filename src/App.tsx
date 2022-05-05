e063dc7b444c0ea7dc262d8b1739e4e66a9c6f67
stedyyulius <stedyyulius@gmail.com>
2022-05-05 20:48:56 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Navbar, Footer, Welcome, Services, Transactions } from './components';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+        <Services />
+        <Transactions />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Transactions.tsx
@@ -0,0 +1,5 @@
+export const Transactions = () => {
+    return (
+        <h1>Transactions</h1>
+    )
+}
